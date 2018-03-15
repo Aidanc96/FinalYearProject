f61eaa07524e9da71e6f033af657a9b28ab4007c
Aidanc96 <www.aidan96@live.co.uk>
2018-03-15 18:21:01 +0000

    created three files for login Auth ready to be pulled from my porject

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+var config = {
+	apiKey: "AIzaSyAi39N_YePWXuPf-hwHdY1Xo-0PceEb2u8",
+	authDomain: "ac-automotive.firebaseapp.com",
+	databaseURL: "https://ac-automotive.firebaseio.com",
+	projectId: "ac-automotive",
+	storageBucket: "",
+	messagingSenderId: "383961438871"
+};
+firebase.initializeApp(config);
+
+const auth = firebase.auth();
+
+export { auth, firebase };
