8a01d770f5bb7a1f525e217f9c0c5b69085b3823
Aidanc96 <www.aidan96@live.co.uk>
2018-07-16 19:48:41 +0100

    moved logo and botton tolanding page

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -3,30 +3,31 @@ import React from "react";
 import LandingFeedDisplay from "../feed/landingFeedDisplay";
 import { Link } from "react-router-dom";
 
-import bgVideo from "../backgroundImage/mountain_car_edit.mp4";
+//import bgVideo from "../backgroundImage/mountain_car_edit.mp4";
+import bgVideo from "../backgroundImage/Only_You_(Car__Edit).mp4";
 import "../css/landingPage.css";
 
-import Button from "material-ui/Button";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 import Header from "../Layouts/Header";
+
+import Paper from "material-ui/Paper";
 
 class LandingPage extends React.Component {
 	render() {
 		return (
 			<div>
-				<video className="welcomeVideo" loop autoPlay muted>
+				<video className="welcomeVideo" loop autoPlay>
 					<source src={bgVideo} type="video/mp4" />
 				</video>
 
 				<div className="welcome" align="center">
-					<h1 className="welcomeText">AC AutoMotive</h1>
-
-					<Button className="welcomeBtn">
-						<Link
-							className="welcomeBtnTxt"
-							to="/login"
-							style={styles.registerBtn}
-						>
+					<Typography variant="display3" color="default" className="logoText">
+						AC AutoMotive
+					</Typography>
+					<Button variant="raised" className="welcomeBtn">
+						<Link className="welcomeBtnTxt" to="/login">
 							LogIn
 						</Link>
 					</Button>
