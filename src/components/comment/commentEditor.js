61870bd71c4cf7b340eda5760db067985e997895
Aidan.c96 <www.aidan96@live.co.uk>
2018-10-29 15:21:14 +0000

    fixed post image display issue

--- src/components/comment/commentEditor.js
+++ src/components/comment/commentEditor.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { TextField, Button } from "material-ui";
 import PropTypes from "prop-types";
 
-import "../css/postEditor.css";
+import "../css/comment.css";
 
 import firebase from "firebase";
 
@@ -10,109 +10,110 @@ import Card, { CardActions, CardContent, CardMedia } from "material-ui/Card";
 import { db, auth } from "../../firebase";
 
 class CommentEditor extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			commentText: "",
-			username: ""
-		};
+    this.state = {
+      commentText: "",
+      username: ""
+    };
 
-		this.handleCommentInputChange = this.handleCommentInputChange.bind(this);
-	}
+    this.handleCommentInputChange = this.handleCommentInputChange.bind(this);
+  }
 
-	componentDidMount() {
-		firebase
-			.database()
-			.ref("users/" + firebase.auth().currentUser.uid)
-			.once("value")
-			.then(snapshot => {
-				this.setState({
-					username: snapshot.val() && snapshot.val().username
-				});
-			});
-	}
+  componentDidMount() {
+    firebase
+      .database()
+      .ref("users/" + firebase.auth().currentUser.uid)
+      .once("value")
+      .then(snapshot => {
+        this.setState({
+          username: snapshot.val() && snapshot.val().username
+        });
+      });
+  }
 
-	handleCommentInputChange = event => {
-		this.setState({
-			commentText: event.target.value
-		});
-	};
+  handleCommentInputChange = event => {
+    this.setState({
+      commentText: event.target.value
+    });
+  };
 
-	//	createComment = () => {
-	//		this.props.createComment(
-	//		this.state.postMedia,
-	//		this.state.postText,
-	//		this.state.username
-	//		);
-	//		this.setState({
-	//			username: "",
-	//			postMeida: "",
-	//			postText: ""
-	//		});
-	//this.setState({ username: username });
-	//	};
+  //	createComment = () => {
+  //		this.props.createComment(
+  //		this.state.postMedia,
+  //		this.state.postText,
+  //		this.state.username
+  //		);
+  //		this.setState({
+  //			username: "",
+  //			postMeida: "",
+  //			postText: ""
+  //		});
+  //this.setState({ username: username });
+  //	};
 
-	createComment = () => {
-		this.props.createComs(this.state.commentText, this.state.username);
-		this.setState({
-			commentText: "",
-			username: ""
-		});
-	};
+  createComment = () => {
+    this.props.createComs(this.state.commentText, this.state.username);
+    this.setState({
+      commentText: "",
+      username: ""
+    });
+  };
 
-	//	createComment = () => {
-	//	this.setState({
-	//		commentText: this.state.commentText,
-	//		username: this.state.username
-	//	}).then(snapshot => {
-	//		this.setState({});
-	//	});
-	//	console.log(this.state.commentText);
-	//	console.log(this.state.username);
-	//};
+  //	createComment = () => {
+  //	this.setState({
+  //		commentText: this.state.commentText,
+  //		username: this.state.username
+  //	}).then(snapshot => {
+  //		this.setState({});
+  //	});
+  //	console.log(this.state.commentText);
+  //	console.log(this.state.username);
+  //};
 
-	//	createComment(commentText, username) {
-	//const postToSave = { commentText };
-	//	const commentToSave = { commentText, username };
-	//	firebase
-	//		.database()
-	//		.ref("allPosts/" + "/comments")
-	//		.push()
-	//		.set(commentToSave);
-	//	}
+  //	createComment(commentText, username) {
+  //const postToSave = { commentText };
+  //	const commentToSave = { commentText, username };
+  //	firebase
+  //		.database()
+  //		.ref("allPosts/" + "/comments")
+  //		.push()
+  //		.set(commentToSave);
+  //	}
 
-	render() {
-		//	const { authUser } = this.props;
+  render() {
+    //	const { authUser } = this.props;
 
-		const { commentText } = this.state;
+    const { commentText } = this.state;
 
-		const isInvalid = commentText === "";
+    const isInvalid = commentText === "";
 
-		return (
-			<Card className="comment-input">
-				<CardContent className="post-body">
-					<TextField
-						className="post-textField"
-						id="post-text"
-						label="Comment"
-						multiline
-						margin="normal"
-						value={this.state.commentText}
-						onChange={this.handleCommentInputChange}
-					/>
-				</CardContent>
-				<Button
-					className="btn-comment"
-					variant="raised"
-					onClick={this.createComment}
-					disabled={isInvalid}
-				>
-					Comment
-				</Button>
-			</Card>
-		);
-	}
+    return (
+      //	<Card className="comment-input">
+      //	<CardContent className="post-body">
+      <div className="comment">
+        <TextField
+          className="post-textField"
+          id="post-text"
+          label="Comment"
+          multiline
+          margin="normal"
+          value={this.state.commentText}
+          onChange={this.handleCommentInputChange}
+        />
+
+        <Button
+          className="btn-comment"
+          variant="raised"
+          onClick={this.createComment}
+          disabled={isInvalid}
+        >
+          Comment
+        </Button>
+      </div>
+    );
+  }
 }
 
 const authCondition = authUser => !!authUser;
--- src/components/post/allPostsContent.js
+++ src/components/post/allPostsContent.js
@@ -9,50 +9,51 @@ import Divider from "material-ui/Divider";
 import ExpandMoreIcon from "material-ui-icons/ExpandMore";
 import Collapse from "material-ui/transitions/Collapse";
 import Card, {
-	CardHeader,
-	CardMedia,
-	CardContent,
-	CardActions
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions
 } from "material-ui/Card";
+import Typography from "@material-ui/core/Typography";
 class AllPostsContent extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		//{console.log(JSON.stringify("The body"+this.props.postBody))}
-		//console.log("recalled content");
-		const { postMedia, postText, allUsersPosts, username } = this.props;
-		//	const Count = require("react-count").OnlineCount;
-		//console.log(this.props.postMedia);
-		return (
-			<div>
-				{allUsersPosts
-					.map((allPost, postIndex) => (
-						<div key={postIndex}>
-							<Card className="post-received">
-								<CardHeader title={allPost.username} />
-								<Divider />
+  render() {
+    //{console.log(JSON.stringify("The body"+this.props.postBody))}
+    //console.log("recalled content");
+    const { postMedia, postText, allUsersPosts, username } = this.props;
+    //	const Count = require("react-count").OnlineCount;
+    //console.log(this.props.postMedia);
+    return (
+      <div>
+        {allUsersPosts
+          .map((allPost, postIndex) => (
+            <div key={postIndex}>
+              <Card className="post-received">
+                <CardHeader title={allPost.username} />
+                <Divider />
 
-								<CardContent className="post-content">
-									<div className="station">
-										<img className="feed-image" src={allPost.postMedia} />
+                <CardContent className="post-content">
+                  <div className="station">
+                    <img className="feed-image" src={allPost.postMedia} />
 
-										<Divider />
-										<div>
-											<h3>{allPost.postText}</h3>
-											<Divider />
-										</div>
-									</div>
-								</CardContent>
-								<CommentFeed />
-							</Card>
-						</div>
-					))
-					.reverse()}
-			</div>
-		);
-	}
+                    <div>
+                      <Typography variant="headline" color="default">
+                        {allPost.postText}
+                      </Typography>
+                    </div>
+                  </div>
+                </CardContent>
+                <CommentFeed />
+              </Card>
+            </div>
+          ))
+          .reverse()}
+      </div>
+    );
+  }
 }
 
 export default AllPostsContent;
